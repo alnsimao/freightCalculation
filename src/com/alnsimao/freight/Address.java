b071d003d78f178511a92601c964219006ba8462
alnsimao <alansimao9a1@gmail.com>
2025-08-20 18:59:40 -0300

    new feature(Add atributtes for the class Address and the Getters and Setters)

--- src/com/alnsimao/freight/Address.java
+++ src/com/alnsimao/freight/Address.java
@@ -1,5 +1,32 @@
 package com.alnsimao.freight;
 
 public class Address {
+ private String streetName;
+ private int number;
+ 
 
+ public Address(String streetName, int number) {
+	 this.number = number;
+	 this.streetName = streetName;
+ }
+
+
+public String getStreetName() {
+	return streetName;
+}
+
+
+public void setStreetName(String streetName) {
+	this.streetName = streetName;
+}
+
+
+public int getNumber() {
+	return number;
+}
+
+
+public void setNumber(int number) {
+	this.number = number;
+}
 }
